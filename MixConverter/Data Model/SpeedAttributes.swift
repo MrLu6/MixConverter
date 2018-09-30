b9bd6c54575f04cb400a6f130f8b0f948a5d7a61
MrLu6 <lcj823063955@gmail.com>
2018-09-30 05:38:36 -0700

    Documented TemperatureAttributes.swift TimeAttributes.swift and SpeedAttributes.swift

--- MixConverter/Data Model/SpeedAttributes.swift
+++ MixConverter/Data Model/SpeedAttributes.swift
@@ -8,22 +8,33 @@
 
 import UIKit
 
+/**
+ This class hold all Speed Unit related attributes
+ */
 class SpeedAttributes{
     
+    /// Names of all speed units
     let SPEED_UNITS_ARRAY = ["Meter per second", "Kilometer per hour", "Foot per second,", "Miles per hour", "Knot"]
     
+    /// Abbreviation of speed time units
     let SPEED_UNITS_SHORT_ARRAY = ["m/s", "km/hr", "ft/s", "mile/hr", "Knot"]
     
+    /// Meter per second ratio to all speed unit in the order which list in SPEED_UNITS_ARRAY
     let METER_PER_SECOND_RATIO = [1, 3.6, 1/0.0254/12, 3600/(1/(1/0.0254/12/3/1760)), 1/(1.852*(1/3.6))] as [Double]
     
+    /// Kilometer per hour ratio to all speed unit in the order which list in SPEED_UNITS_ARRAY
     let KILO_METER_PER_HOUR_RATIO = [1/3.6, 1, 1/3.6*(1/0.0254/12), 1/0.0000254/12/3/1760, 1/(1.852)] as [Double]
     
+    /// Foot per second ratio to all speed unit in the order which list in SPEED_UNITS_ARRAY
     let FOOT_PER_SECOND_RATIO = [0.0254 * 12, 1/(1/3.6*(1/0.0254/12)), 1, 3600/5280, 1/(1.852 * 1/3.6*(1/0.0254/12))] as [Double]
     
+    /// Miles per hour ratio to all speed unit in the order which list in SPEED_UNITS_ARRAY
     let MILE_PER_HOUR_RATIO = [1/(3600/(1/(1/0.0254/12/3/1760))), 1/(1/0.0000254/12/3/1760), 1/(3600/5280), 1, 1/(1.852 * 1/0.0000254/12/3/1760)] as [Double]
     
+    /// Knot per second ratio to all speed unit in the order which list in SPEED_UNITS_ARRAY
     let KNOT_RATIO = [1.852*(1/3.6),1.852,1.852 * 1/3.6*(1/0.0254/12), 1.852 * 1/0.0000254/12/3/1760, 1 ] as [Double]
     
+    /// Singleton - instance of SpeedAttributes()
     static let instance = SpeedAttributes()
     
 }
--- MixConverter/Data Model/TemperatureAttributes.swift
+++ MixConverter/Data Model/TemperatureAttributes.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
-    
+
+/**
+ This class hold all Temperature Unit related attributes
+ */
 class TemperatureAttributes{
     
+    /// Names of all temperature units
     let TEMPERATURE_UNITS_ARRAY = ["Celsius", "Fahrenheit", "Kelvin"]
     
+    /// Abbreviation of all temperature units
     let TEMPERATURE_UNITS_SHORT_ARRAY = ["°C", "°F", "K"]
     
+    /// Singleton - instance of TemperatureAttributes()
     static let instance = TemperatureAttributes()
 }
